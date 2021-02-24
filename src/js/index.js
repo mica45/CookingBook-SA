0b761674e65c8b33b38b52d3f4be9228820efcb3
ALRND <dackralp@gmail.com>
2021-02-24 09:40:27 +0100

    added getRecipe to IngredList

--- src/js/index.js
+++ src/js/index.js
@@ -65,7 +65,7 @@ function getMealList() {
                         </div>
                         <div class = "meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                            <a href = "#" class = "recipe-btn" onclick="getIngredRecipe(${meal.idMeal})">Get Recipe</a>
                         </div>
                     </div>
                 `;
@@ -119,6 +119,80 @@ function getMeal() {
 function getRecipe() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + searchInputTxt)
+        .then(res => res.json())
+        .then(res => {
+            createMeal(res.meals[0]);
+        })
+        .catch(e => {
+            console.warn(e);
+        });
+
+    const createMeal = meal => {
+        const ingredients = [];
+
+        // Get all ingredients from the object. Up to 20
+        for (let i = 1; i <= 20; i++) {
+            if (meal[`strIngredient${i}`]) {
+                ingredients.push(
+                    `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+                );
+            } else {
+                // Stop if there are no more ingredients
+                break;
+            }
+        }
+
+        const newInnerHTML = `
+                <div class="row">
+                    <div class="columns five">
+                        <img class="recipe-image"src="${meal.strMealThumb}" alt="Meal Image">
+                        ${
+                            meal.strCategory
+                                ? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
+                                : ''
+                        }
+                        ${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+                        ${
+                            meal.strTags
+                                ? `<p><strong>Tags:</strong> ${meal.strTags
+                                        .split(',')
+                                        .join(', ')}</p>`
+                                : ''
+                        }
+                        <h5>Ingredients:</h5>
+                        <ul>
+                            ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+                        </ul>
+                    </div>
+                    <div class="columns seven">
+                        <h4>${meal.strMeal}</h4>
+                        <p>${meal.strInstructions}</p>
+                    </div>
+                </div>
+                ${
+                    meal.strYoutube
+                        ? `
+                <div class="row">
+                    <h5>Video Recipe</h5>
+                    
+                    <div class="videoWrapper">
+                        <iframe width="420" height="315"
+                        src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+                        </iframe>
+                    </div>
+                </div>`
+                        : ''
+                }
+            `;
+
+        mealRecipe.innerHTML = newInnerHTML;
+    };
+
+};
+
+function getIngredRecipe(mealId) {
+    console.log("getIngred has been triggered: mealId= " + mealId)
+    fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + mealId)
         .then(res => res.json())
         .then(res => {
             createMeal(res.meals[0]);
